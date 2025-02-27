acc59349165b0f2b3970347ed952d4cadaa18836
Oliver Ward <oliver.ward@iki.fi>
2025-02-27 11:19:34 +0200

    Day 3

---
+++ day-03/src/main.rs
@@ -0,0 +1,68 @@
+use std::fs;
+
+
+const TESTING: bool = false;
+const INPUT: &str = if TESTING { 
+    "sample.txt"
+} else {
+    "input.txt"
+};
+
+fn get_inputs(path: &str) -> String {
+    fs::read_to_string(path).expect("lol")
+}
+
+fn get_cmds(data: &str) -> Vec<&str> {
+    data.split("mul").collect::<Vec<&str>>()
+}
+
+static mut ENABLED: bool = true;
+
+fn is_valid(cmd: &str) -> bool {
+    let mut enabled_after_cmd = true;
+    unsafe {
+        if cmd.contains("don't()") {
+            enabled_after_cmd = false;
+        }
+        if cmd.contains("do()") {
+            if !ENABLED {
+                ENABLED = true;
+                return false;
+            }
+        }
+        if cmd.chars().nth(0) == Some('(') && cmd.contains(')') && ENABLED {
+            ENABLED = enabled_after_cmd;
+            return cmd.find(')').is_some()
+        }
+    }
+    false
+}
+
+fn get_mul(cmd: &str) -> u64 {
+    let end = cmd.find(')').unwrap();
+    let nums = cmd.get(1..end).unwrap();
+    let product = nums
+        .split(',')
+        .into_iter()
+        .map(|i| i.parse::<u64>().unwrap_or(0))
+        .product();
+
+    product
+}
+
+
+fn main() {
+    let data = get_inputs(INPUT);
+    let mul_cmds = get_cmds(&data);
+    let valid_instructions = mul_cmds
+        .into_iter()
+        .filter(|&cmd| is_valid(cmd))
+        .collect::<Vec<&str>>();
+
+    let sum = valid_instructions
+        .iter()
+        .map(|&cmd| get_mul(cmd))
+        .sum::<u64>();
+
+    println!("{}", sum);
+}
