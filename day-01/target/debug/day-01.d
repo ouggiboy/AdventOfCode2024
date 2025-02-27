c3f078eddd552d87454646c4719e7ed68dc5cdf2
Oliver Ward <oliver.ward@iki.fi>
2025-02-27 10:52:31 +0200

    Day 1

---
+++ day-01/target/debug/day-01.d
@@ -0,0 +1,1 @@
+C:\Rust\AdventOfCode2024\day-01\target\debug\day-01.exe: C:\Rust\AdventOfCode2024\day-01\src\main.rs
---
+++ day-01/target/debug/deps/day_01-bd0c496ee251fbb6.d
@@ -0,0 +1,5 @@
+C:\Rust\AdventOfCode2024\day-01\target\debug\deps\libday_01-bd0c496ee251fbb6.rmeta: src\main.rs
+
+C:\Rust\AdventOfCode2024\day-01\target\debug\deps\day_01-bd0c496ee251fbb6.d: src\main.rs
+
+src\main.rs:
