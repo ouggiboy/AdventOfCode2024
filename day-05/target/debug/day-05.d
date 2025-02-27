ef4fea776961f4ccb6fe43fa09e1fa8bebd0cb48
ouggiboy <136275595+ouggiboy@users.noreply.github.com>
2025-02-27 14:18:38 +0200

    Delete day-05/target directory

--- day-05/target/debug/day-05.d
+++
@@ -1,1 +0,0 @@
-C:\Rust\AdventOfCode2024\day-05\target\debug\day-05.exe: C:\Rust\AdventOfCode2024\day-05\src\main.rs
--- day-05/target/debug/deps/day_05-604919b08d63e403.d
+++
@@ -1,5 +0,0 @@
-C:\Rust\AdventOfCode2024\day-05\target\debug\deps\libday_05-604919b08d63e403.rmeta: src\main.rs
-
-C:\Rust\AdventOfCode2024\day-05\target\debug\deps\day_05-604919b08d63e403.d: src\main.rs
-
-src\main.rs:
