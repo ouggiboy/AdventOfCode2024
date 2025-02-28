98c0c6c08c171d98fd2c8f121b08501c46d6845b
Oliver Ward <oliver.ward@iki.fi>
2025-02-28 11:21:58 +0200

    Reset

--- day-05/target/debug/deps/day_05-eec9eeec5894a242.d
+++
@@ -1,5 +0,0 @@
-C:\Rust\AdventOfCode2024\day-05\target\debug\deps\libday_05-eec9eeec5894a242.rmeta: src\main.rs
-
-C:\Rust\AdventOfCode2024\day-05\target\debug\deps\day_05-eec9eeec5894a242.d: src\main.rs
-
-src\main.rs:
