6e217bfa2d1455a31f66e7cc126fc1639117f403
Oliver Ward <oliver.ward@iki.fi>
2025-03-17 13:29:29 +0200

    Part 1 cleanup

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -37,7 +37,7 @@ fn get_distance(a: (usize, usize), b: (usize, usize)) -> (usize, usize) {
     (a.0.abs_diff(b.0), a.1.abs_diff(b.1))
 }
 
-fn check_if_node_valid(pos: (usize, usize), nodemap: &mut Vec<(usize, usize)>) -> bool {
+fn node_valid(pos: (usize, usize), nodemap: &mut Vec<(usize, usize)>) -> bool {
     if nodemap.contains(&pos) {
         return false;
     }
@@ -47,54 +47,70 @@ fn check_if_node_valid(pos: (usize, usize), nodemap: &mut Vec<(usize, usize)>) -
 
 // return value indicates if we added no new antinodes, or 1 or 2 new ones
 // 'a' is always the first antenna
-fn mark_antinodes(a: (usize, usize), b: (usize, usize), width: usize, height: usize, nodemap: &mut Vec<(usize, usize)>) -> (bool, bool) {
+fn mark_antinodes(a: &mut (usize, usize), b: &mut (usize, usize), width: usize, height: usize, nodemap: &mut Vec<(usize, usize)>) -> u32 {
 
-    let (mut n1, mut n2) = (false, false);
+    let mut nodecount = 0;
 
-    let d = get_distance(a, b);
+    let d = get_distance(*a, *b);
 
     if d == (0, 0) {
-        return (n1, n2);
+        return nodecount;
     }
     
     // if on same vertical line
     if d.0 == 0 {
         if a.1 >= d.1 {
-            n1 = check_if_node_valid((a.0, a.1 - d.1), nodemap);
+            if node_valid((a.0, a.1 - d.1), nodemap) {
+                nodecount += 1;
+            }
         }
         if b.1 < height - d.1 {
-            n2 = check_if_node_valid((b.0, b.1 + d.1), nodemap);
+            if node_valid((b.0, b.1 + d.1), nodemap) {
+                nodecount += 1;
+            }
         }
     }
     // if on same horizontal line
     else if d.1 == 0 {
         if a.0 >= d.0 {
-            n1 = check_if_node_valid((a.0 - d.0, a.1), nodemap);
+            if node_valid((a.0 - d.0, a.1), nodemap) {
+                nodecount += 1;
+            }
         }
         if b.0 < width - d.0 {
-            n2 = check_if_node_valid((b.0 + d.0, b.1), nodemap);
+            if node_valid((b.0 + d.0, b.1), nodemap) {
+                nodecount += 1;
+            }
         }
     }
     // if a more left than b
     else if a.0 < b.0 {
         if a.1 >= d.1 && a.0 >= d.0 {
-            n1 = check_if_node_valid((a.0 - d.0, a.1 - d.1), nodemap);
+            if node_valid((a.0 - d.0, a.1 - d.1), nodemap) {
+                nodecount += 1;
+            }
         }
         if b.1 < height - d.1 && b.0 < width - d.0 {
-            n2 = check_if_node_valid((b.0 + d.0, b.1 + d.1), nodemap);
+            if node_valid((b.0 + d.0, b.1 + d.1), nodemap) {
+                nodecount += 1;
+            }
         }
     }
     // if a more right than b
     else if a.0 > b.0 {
         if a.1 >= d.1 && a.0 < width - d.0 {
-            n1 = check_if_node_valid((a.0 + d.0, a.1 - d.1), nodemap);
+            if node_valid((a.0 + d.0, a.1 - d.1), nodemap) {
+                nodecount += 1;
+            }
         }
         if b.1 < height - d.1 && b.0 >= d.0 {
-            n2 = check_if_node_valid((b.0 - d.0, b.1 + d.1), nodemap);
+            if node_valid((b.0 - d.0, b.1 + d.1), nodemap) {
+                nodecount += 1;
+            }
         }
     }
 
-    (n1, n2)
+    nodecount
 }
 
 fn main() {
@@ -107,17 +123,12 @@ fn main() {
 
     for y in 0..height {
         for x in 0..width {
-            if map[y][x] != '.' && map[y][x] != '#' {
-                let v = find_matches(map[y][x], (x, y), &map, width, height); {
-                    if v.is_empty() { 
-                        continue; 
-                    }
-                    else {
-                        for pair in v {
-                            let ans = mark_antinodes((x, y), pair, width, height, &mut nodemap);
-                            if ans.0 { unique_nodes += 1; }
-                            if ans.1 { unique_nodes += 1; }
-                        }
+            if map[y][x] != '.' {
+                let character = map[y][x];
+                let mut start_pos = (x, y);
+                let v = find_matches(character, start_pos, &map, width, height); {
+                    for mut pair in v {
+                        unique_nodes += mark_antinodes(&mut start_pos, &mut pair, width, height, &mut nodemap);
                     }
                 }
             }
