79a079b6c97faa19b00adddb048370d8933f9531
Oliver Ward <oliver.ward@iki.fi>
2025-03-25 13:06:01 +0200

    Parsing function done, I honestly have no idea if there is a better way to do this

---
+++ day-14/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+
+#[cfg(debug_assertions)]
+const PATH: &str = "sample.txt";
+
+#[cfg(not(debug_assertions))]
+const PATH: &str = "input.txt";
+
+mod part1;
+// mod part2;
+
+fn parse_input(path: &str) -> Vec<[(i64, i64); 2]> {
+    let pair_map: Vec<Vec<(i64, i64)>> = fs::read_to_string(path).unwrap()
+        .lines()
+        .map(|x| { x.split_ascii_whitespace()
+            .map(|half| { 
+                let mut iter = half.split(',');
+                let x: i64 = iter.next().unwrap()[2..].parse().unwrap();
+                let y: i64 = iter.next().unwrap().parse().unwrap();
+                (x, y)
+            })
+            .collect()
+        })
+        .collect();
+    let mut pairs: Vec<[(i64, i64); 2]> = Vec::new();
+    for pair in pair_map {
+        pairs.push([pair[0], pair[1]]);
+    }
+    pairs
+}
+
+fn main() {
+    let input = parse_input(PATH);
+    part1::run(&input);
+    // part2::run(&input);
+}
---
+++ day-14/src/part1.rs
@@ -0,0 +1,5 @@
+
+
+pub fn run(input: &Vec<[(i64, i64); 2]>) {
+    println!("{:?}", input);
+}
