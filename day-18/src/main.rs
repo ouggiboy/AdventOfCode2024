3090f0a1618fc72c56caa35c86eb54277eefb067
Oliver Ward <oliver.ward@iki.fi>
2025-04-03 11:04:08 +0300

    Day 18 done, was nice and easy after day 17

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -11,7 +11,7 @@ const MAX_COORDS: usize = 70;
 const PATH: &str = "input.txt";
 
 mod part1;
-// mod part2;
+mod part2;
 
 fn parse_input(path: &str) -> Vec<(usize, usize)> {
     let input: String = fs::read_to_string(path).unwrap();
@@ -26,5 +26,5 @@ fn parse_input(path: &str) -> Vec<(usize, usize)> {
 fn main() {
     let input = parse_input(PATH);
     part1::run(&input);
-    // part2::run(&input);
+    part2::run(&input);
 }
--- day-18/src/part2.rs
+++ day-18/src/part2.rs
@@ -2,13 +2,13 @@ use std::collections::HashSet;
 
 use crate::MAX_COORDS;
 
-fn find_path(grid: &[[bool; MAX_COORDS + 1]; MAX_COORDS + 1]) -> Option<u64> {
-    let mut queue = Vec::from([(0i32, 0i32, 0)]);
+fn find_path(grid: &[[bool; MAX_COORDS + 1]; MAX_COORDS + 1]) -> bool {
+    let mut queue = Vec::from([(0i32, 0i32)]);
 
     let mut seen = HashSet::from([(0, 0)]);
 
     while !queue.is_empty() {
-        let (x, y, steps) = queue.remove(0);
+        let (x, y) = queue.remove(0);
         for (nx, ny) in [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)] {
             if nx < 0 || ny < 0 {
                 continue;
@@ -25,24 +25,23 @@ fn find_path(grid: &[[bool; MAX_COORDS + 1]; MAX_COORDS + 1]) -> Option<u64> {
                 continue;
             }
             if (nx, ny) == (MAX_COORDS, MAX_COORDS) {
-                return Some(steps + 1);
+                return true;
             }
             seen.insert((nx, ny));
-            queue.push((nx as i32, ny as i32, steps + 1));
+            queue.push((nx as i32, ny as i32));
         }
     }
-    None
+    false
 }
 
 pub fn run(input: &Vec<(usize, usize)>) {
     let mut grid = [[true; MAX_COORDS + 1]; MAX_COORDS + 1];
+    let mut i = 0;
 
-    for i in 0..1024 {
+    while find_path(&grid) {
         grid[input[i].1][input[i].0] = false;
+        i += 1;
     }
     
-    if let Some(steps) = find_path(&grid) {
-        println!("Part 1: {}", steps);
-    }
-
+    println!("Part 2: {},{}", input[i-1].0, input[i-1].1);
 }
