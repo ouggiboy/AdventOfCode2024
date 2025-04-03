57d837b6a454f43b112b7b7f9fc47a6a7f678252
Oliver Ward <oliver.ward@iki.fi>
2025-04-03 10:33:12 +0300

    Made a more optimized pathfinder

--- day-18/src/part1.rs
+++ day-18/src/part1.rs
@@ -1,51 +1,51 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 use crate::MAX_COORDS;
 
-fn find_path(grid: &[[bool; MAX_COORDS + 1]; MAX_COORDS + 1], start: (usize, usize), end: (usize, usize), seen: &mut HashMap<(usize, usize), u64>, steps: u64, min_steps: &mut u64) {
-    if start == end {
-        if steps < *min_steps {
-            *min_steps = steps;
-        }
-        return;
-    }
-    let directions = [(1, 0), (0, 1), (0, -1), (-1, 0)];
-    for (vx, vy) in directions {
-        let x = start.0 as i64 + vx;
-        let y = start.1 as i64 + vy;
-        if x < 0 || y < 0 || x as usize > MAX_COORDS || y as usize > MAX_COORDS {
-            continue;
-        }
-        let x = x as usize;
-        let y = y as usize;
-        if grid[y][x] == false {
-            continue;
-        }
-        if let Some(&old_steps) = seen.get(&(x, y)) {
-            if old_steps > steps {
-                seen.insert((x, y), steps);
-            }
-            else {
+fn find_path(grid: &[[bool; MAX_COORDS + 1]; MAX_COORDS + 1]) -> Option<u64> {
+    let mut queue = Vec::from([(0i32, 0i32, 0)]);
+
+    let mut seen = HashSet::from([(0, 0)]);
+
+    while !queue.is_empty() {
+        let (x, y, steps) = queue.remove(0);
+        for (nx, ny) in [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)] {
+            if nx < 0 || ny < 0 {
                 continue;
             }
+            let nx = nx as usize;
+            let ny = ny as usize;
+            if nx > MAX_COORDS || ny > MAX_COORDS {
+                continue;
+            }
+            if grid[ny][nx] == false {
+                continue;
+            }
+            if seen.contains(&(nx, ny)) {
+                continue;
+            }
+            if (nx, ny) == (MAX_COORDS, MAX_COORDS) {
+                return Some(steps + 1);
+            }
+            seen.insert((nx, ny));
+            queue.push((nx as i32, ny as i32, steps + 1));
         }
-        else {
-            seen.insert((x, y), steps);
-        }
-        find_path(grid, (x, y), end, seen, steps + 1, min_steps);
     }
+    None
 }
 
 pub fn run(input: &Vec<(usize, usize)>) {
+    let start = std::time::Instant::now();
+
     let mut grid = [[true; MAX_COORDS + 1]; MAX_COORDS + 1];
     for i in 0..1024 {
         grid[input[i].1][input[i].0] = false;
     }
     
-    let mut seen = HashMap::new();
-    let mut min_steps = u64::MAX;
+    if let Some(steps) = find_path(&grid) {
+        let end = std::time::Instant::now();
+        println!("Part 1: {}", steps);
+        println!("Total time: {:?}", end - start);
+    }
 
-    find_path(&grid, (0, 0), (MAX_COORDS, MAX_COORDS), &mut seen, 0, &mut min_steps);
-
-    println!("Part 1: {}", min_steps);
 }
